739ceb946f446cf1aa9c52237aff49396d71225d
Vikasini <156224575+VikasiniM@users.noreply.github.com>
2024-12-23 18:06:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// Initialize EmailJS
+
 (function () {
-    emailjs.init("yDHVEKGvMNJuFL-XH"); // Replace "YOUR_USER_ID" with your EmailJS User ID
+    emailjs.init("yDHVEKGvMNJuFL-XH"); 
 })();
 
-// Function to send mail
+
 function sendMail() {
-    // Collect form data
+    
     const name = document.getElementById("name").value;
     const phone = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
-    // Validate form inputs
+    
     if (!name || !phone || !email || !message) {
         alert("Please fill out all fields!");
         return;
     }
 
-    // Prepare the template parameters
+    
     const templateParams = {
         name: name,
         phone: phone,
@@ -25,7 +25,7 @@ function sendMail() {
         message: message,
     };
 
-    // Send email using EmailJS
+   
     emailjs
         .send("service_0y4wa67", "template_mx0y58o", templateParams)
         .then((response) => {
@@ -40,14 +40,14 @@ function sendMail() {
 
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+        
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -55,25 +55,25 @@ $(document).ready(function(){
         }
     });
      
-    // slide-up script
+   
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+        
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+        
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
+    
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
+    
     var typed = new Typed(".typing", {
         strings: ["Software Developer💻", "Coder","Problem solver","Graduate🎓"],
         typeSpeed: 100,
@@ -88,7 +88,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
+    
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
